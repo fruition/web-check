4d4c44f7c68de297f526f507ffd38008927ca646
Jorgihno Ojeda <77262595+yoryidev@users.noreply.github.com>
2024-03-29 10:53:59 -0300

    Revert "Feat/update style"

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,22 +1,23 @@
+
 const colors = {
-  primary: "#2065f8",
-  primaryLighter: "#02b2fe",
-  textColor: "#202129",
-  textColorSecondary: "#13216c",
-  background: "#ffffff",
-  backgroundDarker: "#ffffff",
-  backgroundLighter: "#f5f6fc",
-  bgShadowColor: "#f0f1f1",
-  fgShadowColor: "#f5f6fc",
-  primaryTransparent: "#2065f812",
+  primary: '#9fef00',
+  primaryLighter: '#cff97a',
+  textColor: '#ffffff',
+  textColorSecondary: '#a4b1cd',
+  background: '#141d2b',
+  backgroundDarker: '#111927',
+  backgroundLighter: '#1a2332',
+  bgShadowColor: '#0f1620',
+  fgShadowColor: '#456602',
+  primaryTransparent: '#9fef0012',
 
   // Action Colors
-  info: "#04e4f4",
-  success: "#20e253",
-  warning: "#f6f000",
-  error: "#fca016",
-  danger: "#f80363",
-  neutral: "#272f4d",
+  info: '#04e4f4',
+  success: '#20e253',
+  warning: '#f6f000',
+  error: '#fca016',
+  danger: '#f80363',
+  neutral: '#272f4d',
 };
 
 export default colors;
