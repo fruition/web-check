769cc9040f920d9aba55c7295bb89c806da3ff9b
yoryidev <jorgihno.ojeda@fruition.net>
2024-04-29 02:36:40 -0300

    Fix: deployment issue

--- src/components/Form/Nav.tsx
+++ src/components/Form/Nav.tsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { StyledCard } from 'components/Form/Card';
-import Heading from 'components/Form/Heading';
-import colors from 'styles/colors';
-import { ReactNode } from 'react';
+import { StyledCard } from "components/Form/Card";
+import Heading from "components/Form/Heading";
+import colors from "styles/colors";
+import { ReactNode } from "react";
 
 const Header = styled(StyledCard)`
   margin: 1rem auto;
@@ -16,15 +16,15 @@ const Header = styled(StyledCard)`
   width: 95vw;
 `;
 
-const Nav = (props: { children?: ReactNode}) => {
+const Nav = (props: { children?: ReactNode }) => {
   return (
     <Header as="header">
-    <Heading color={colors.primary} size="large">
-      <img width="64" src="/web-check.png" alt="Web Check Icon" />
-      <a href="/">Web Check</a>
-    </Heading>
+      <Heading color={colors.primary} size="large">
+        <img width="64" src="/web-check.png" alt="Web Check Icon" />
+        <a href="/">Web Check</a>
+      </Heading>
       {props.children && props.children}
-  </Header>
+    </Header>
   );
 };
 
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,11 @@
+import styled from "styled-components";
 
-import styled from 'styled-components';
-
-import colors from 'styles/colors';
-import Heading from 'components/Form/Heading';
-import Footer from 'components/misc/Footer';
-import Nav from 'components/Form/Nav';
-import Button from 'components/Form/Button';
-import { StyledCard } from 'components/Form/Card';
+import colors from "styles/colors";
+import Heading from "components/Form/Heading";
+import Footer from "components/misc/Footer";
+import Nav from "components/Form/Nav";
+import Button from "components/Form/Button";
+import { StyledCard } from "components/Form/Card";
 
 const AboutContainer = styled.div`
   width: 95vw;
@@ -41,26 +40,33 @@ const NotFoundInner = styled(StyledCard)`
   align-items: center;
   margin: 1rem;
   gap: 0.5rem;
-  h2 { font-size: 8rem; }
+  h2 {
+    font-size: 8rem;
+  }
 `;
-
 
 const NotFound = (): JSX.Element => {
   return (
     <>
-    <AboutContainer>
-    <Nav />
-    <NotFoundInner>
-      <Heading as="h2" size="large" color={colors.primary}>404</Heading>
-      <span className="im-drink">🥴</span>
-      <Heading as="h3" size="large" color={colors.primary}>Not Found</Heading>
-      <HeaderLinkContainer>
-        <a href="/"><Button>Back to Homepage</Button></a>
-      </HeaderLinkContainer>
-      <a href="https://github.com/lissy93/web-check">Report Issue</a>
-    </NotFoundInner>
-    </AboutContainer>
-    <Footer isFixed={true} />
+      <AboutContainer>
+        <Nav />
+        <NotFoundInner>
+          <Heading as="h2" size="large" color={colors.primary}>
+            404
+          </Heading>
+          <span className="im-drink">🥴</span>
+          <Heading as="h3" size="large" color={colors.primary}>
+            Not Found
+          </Heading>
+          <HeaderLinkContainer>
+            <a href="/">
+              <Button>Back to Homepage</Button>
+            </a>
+          </HeaderLinkContainer>
+          <a href="https://github.com/lissy93/web-check">Report Issue</a>
+        </NotFoundInner>
+      </AboutContainer>
+      <Footer isFixed={true} />
     </>
   );
 };
